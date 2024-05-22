d53bbb05f9e5c66771410386a126973c82bb9a09
githukelvin <kelvingithu019@gmail.com>
2024-05-22 09:23:01 +0300

    resolved build issue of contracts

--- src/utils/deploy.js
+++ src/utils/deploy.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3'
-import RegistryABI from '../../../hardhat/artifacts/contracts/LandRegistry.sol/LandRegistry.json' assert { type: 'json' }
-import SaleABI from '../../../hardhat/artifacts/contracts/LandSale.sol/LandSale.json' assert { type: 'json' }
+import RegistryABI from '../../artifacts/contracts/LandRegistry.sol/LandRegistry.json' assert { type: 'json' }
+import SaleABI from '../../artifacts/contracts/LandSale.sol/LandSale.json' assert { type: 'json' }
 import { environment } from './config.js'
 import * as path from 'node:path'
 import * as fs from 'node:fs'
